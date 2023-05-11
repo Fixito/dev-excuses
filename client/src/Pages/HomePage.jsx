71d210fa061cf73eb797758d8ad53aeb38d1cf23
Thomas ROBERT <thomas_3004@hotmail.fr>
2023-05-11 16:47:40 +0200

    Lift fetch

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from 'react';
 import Generator from '../components/Generator.jsx';
+import autoFetch from '../axios/custom.js';
 
 const HomePage = () => {
+  const [excuse, setExcuse] = useState(null);
+  const [status, setStatus] = useState('idle');
+
+  const fetchRandomExcuse = async () => {
+    setStatus('pending');
+
+    try {
+      const {
+        data: { excuse }
+      } = await autoFetch('/random');
+      setExcuse(excuse);
+      setStatus('resolved');
+    } catch (error) {
+      console.log(error.response.data.msg);
+    }
+  };
+
+  useEffect(() => {
+    fetchRandomExcuse();
+  }, []);
+
+  if (status === 'idle' || status === 'pending') {
+    return (
+      <main className='full-page grid-center'>
+        <div>Loading...</div>
+      </main>
+    );
+  }
+
   return (
     <main className='full-page grid-center'>
-      <Generator />
+      <Generator excuse={excuse} fetchRandomExcuse={fetchRandomExcuse} />
     </main>
   );
 };
--- client/src/components/Generator.jsx
+++ client/src/components/Generator.jsx
@@ -1,29 +1,10 @@
-import { useEffect, useState } from 'react';
 import Button from './Button.jsx';
-import autoFetch from '../axios/custom.js';
 
-const Generator = () => {
-  const [message, setMessage] = useState('');
-
-  const fetchRandomExcuse = async () => {
-    try {
-      const {
-        data: { excuse }
-      } = await autoFetch('/random');
-      setMessage(excuse.message);
-    } catch (error) {
-      console.log(error.response.data.msg);
-    }
-  };
-
-  useEffect(() => {
-    fetchRandomExcuse();
-  }, []);
-
+const Generator = ({ excuse, fetchRandomExcuse }) => {
   return (
     <article className='card'>
       <h1>Dev excuses</h1>
-      <p className='text italic'>{message}</p>
+      <p className='text italic'>{excuse?.message}</p>
       <Button fetchRandomExcuse={fetchRandomExcuse} />
     </article>
   );
