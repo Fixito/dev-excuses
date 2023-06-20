55cd8617fc4e4ff73b3be3703bc31c9217d10bcc
Thomas ROBERT <thomas_3004@hotmail.fr>
2023-06-20 14:41:19 +0200

    Update animations

--- client/src/components/Generator.jsx
+++ client/src/components/Generator.jsx
@@ -4,15 +4,17 @@ import Modal from './Modal.jsx';
 const Generator = ({ excuse, fetchRandomExcuse, status }) => {
   return (
     <article className='card'>
-      <h1 className='fade-in'>Dev excuses</h1>
+      <h1 className='hide fade-in'>Dev excuses</h1>
       {status === 'resolved' ? (
         <p className='text italic'>{excuse?.message}</p>
       ) : (
         <div className='skeleton skeleton-text'></div>
       )}
-      <div className='btn-container slide-from-top'>
-        <Button fetchRandomExcuse={fetchRandomExcuse} />
-        <Modal />
+      <div className='mask'>
+        <div className='btn-container hide slide-down'>
+          <Button fetchRandomExcuse={fetchRandomExcuse} />
+          <Modal />
+        </div>
       </div>
     </article>
   );
